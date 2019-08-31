953d3bed3d7c308e6562fbbdc67c6414a7fc1abb
Adam Michalak <adam_michalak@epam.com>
2019-08-31 12:12:09 +0200

    aggregate documents in two lists

--- src/ProgrammerService.java
+++ src/ProgrammerService.java
@@ -1,10 +1,24 @@
-import pl.blueenergy.document.DocumentDao;
+import pl.blueenergy.document.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ProgrammerService {
 
+
+
 	public void execute(DocumentDao documentDao) {
-		//Miejsce na twój kod:
-		
-		
+		List<Questionnaire> questionnaireList = new ArrayList<>();
+		List<ApplicationForHolidays> applicationForHolidaysList = new ArrayList<>();
+		for(Document document : documentDao.getAllDocumentsInDatabase()){
+			if (document instanceof Questionnaire) {
+				questionnaireList.add((Questionnaire)document);
+			}
+			else if (document instanceof ApplicationForHolidays) {
+				applicationForHolidaysList.add((ApplicationForHolidays)document);
+			}
+		}
 	}
 }
